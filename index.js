531bd073cfc87dab37eea2d520990ce20a235e7c
Stanley Delva <juniordelva1@gmail.com>
2025-01-09 00:02:02 -0500

    was getting error with uuid because it is a library built for Node.js environments and browsers do not the import syntax, as a result I had to use vite

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { showCalendar } from "./calendar.js";
 import { showCompleted } from "./completed.js";
 import { showHistory } from "./history.js";
 import { showNotes, closeAndCreateNotes } from "./note.js";
+import { v4 as uuid } from "uuid";
 
 const tabBtn = document.querySelectorAll(".nav-task");
 const contentSections = document.querySelector(".content-section");
@@ -10,11 +11,13 @@ const contentSections = document.querySelector(".content-section");
 // with some mock data
 const noteStorageArray = [
     {
+        id: uuid(),
         title: "Lion",
         description:
             "The lion is known as the king of the jungle and is a large carnivorous feline native to Africa and parts of Asia.",
     },
     {
+        id: uuid(),
         title: "Elephant",
         description:
             "Elephants are the largest land animals on Earth, known for their intelligence, strong social bonds, and iconic trunks.",
